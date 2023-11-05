acf0da304086945b050a104f5552d0d4f42f9aa7
TheSolid-KANG <tortoise8947@gmail.com>
2023-11-05 19:10:59 +0900

    수정 1. CalebContext.cs 수정: _TCFinancialLedger 추가

--- BecomeCaleb_WEB/Models.CalebTbl/CalebContext.cs
+++ BecomeCaleb_WEB/Models.CalebTbl/CalebContext.cs
@@ -15,6 +15,7 @@ namespace BecomeCaleb_WEB.Models.CalebTbl
         public virtual DbSet<_TCMission> _TCMissions { get; set; } = null!;
         public virtual DbSet<_TCSearchRecord> _TCSearchRecords { get; set; } = null!;
         public virtual DbSet<_TCUser> _TCUsers { get; set; } = null!;
+        public virtual DbSet<_TCFinancialLedger> _TCFinancialLedgers { get; set; } = null!;
 
         public CalebContext() { }//231009: default 생성자를 만들지 않으면 'Unable to resolve service for type 오류'가 발생한다.
         public CalebContext(DbContextOptions<CalebContext> options) : base(options)
@@ -38,6 +39,7 @@ namespace BecomeCaleb_WEB.Models.CalebTbl
             modelBuilder.ApplyConfiguration(new Configurations._TCMissionConfiguration());
             modelBuilder.ApplyConfiguration(new Configurations._TCSearchRecordConfiguration());
             modelBuilder.ApplyConfiguration(new Configurations._TCUserConfiguration());
+            modelBuilder.ApplyConfiguration(new Configurations._TCFinancialLedgerConfiguration());
 
         OnModelCreatingGeneratedProcedures(modelBuilder);
         OnModelCreatingGeneratedFunctions(modelBuilder);
